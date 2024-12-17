7c1a23bd9a2e1251ebe0ee0494c551c5d4e63b64
Isabela Coelho <isabelacoelhoo@gmail.com>
2024-12-17 16:44:38 -0300

    cria tipo CartaType

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,27 @@ import "./App.css";
 import Carta from "./Carta";
 import MaoDeCartas from "./MaoDeCartas";
 
-const MAO:Array<string> = [
-  "Pegar madeira",
-  "Pegar pedra",
-  "Pegar água",
-  "Pegar amoras"
+type CartaType = {
+  id:string;
+  titulo:string;
+  texto:string;
+  custo:number;
+}
+
+const MAO:Array<CartaType> = [
+  {id:"m1", titulo: "Pegar madeira",texto:"Ganha 1 madeira", custo: 0},
+  {id:"a1", titulo: "Pegar água",texto:"Ganha 1 água", custo: 0},
+  {id:"a2", titulo: "Pegar água",texto:"Ganha 1 água", custo: 0},
+  {id:"p1", titulo: "Pegar pedra",texto:"Ganha 1 pedra", custo: 1},
+  {id:"b1", titulo: "Pegar amora",texto:"Ganha 1 amora", custo: 2},
+  {id:"m2", titulo: "Pegar madeira",texto:"Ganha 1 madeira", custo: 0},
 ]
 
 function App() {
   return (
     <>
       <MaoDeCartas>
-        {MAO.map((item)=><Carta key={item} titulo={item}/>)}
+        {MAO.map((item)=><Carta key={item.id} titulo={item.titulo}/>)}
       </MaoDeCartas>
     </>
   );
