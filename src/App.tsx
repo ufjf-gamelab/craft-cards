8031979e6b90ceeca321ed72fb1124b1656026a1
Isabela Coelho <isabelacoelhoo@gmail.com>
2024-12-18 16:53:14 -0300

    adiciona estado pra pontos

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Carta from "./Carta";
 import ListaDeRecursos from "./ListaDeRecursos";
@@ -19,10 +20,17 @@ const MAO:Array<CartaType> = [
   {id:"m2", titulo: "Pegar madeira",texto:"Ganha 1 madeira", custo: 0},
 ]
 
-function App() {
 
+function App() {
+  let [pontos, setPontos] = useState(0);
+  
+  function aumentaPonto(){
+    setPontos(pontos + 1);
+  }
   return (
     <>
+      <div>pontos: {pontos}</div>
+      <button onClick={aumentaPonto}>Aumenta Ponto</button>
       <ListaDeRecursos/>
       <MaoDeCartas>
         {MAO.map((item)=><Carta key={item.id} carta = {item}/>)}
