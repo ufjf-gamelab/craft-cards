0107d2469b599a72835dc2fd1da6d6554a2e0429
Isabela Coelho <isabelacoelhoo@gmail.com>
2024-12-18 16:43:07 -0300

    extrai dados da lista de recursos

--- src/ListaDeRecursos.tsx
+++ src/ListaDeRecursos.tsx
@@ -1,16 +1,24 @@
-import "./ListaDeRecursos.css"
+import "./ListaDeRecursos.css";
+
+export type RecursoType = {
+  nome: string;
+  quantidade: number;
+};
+
+const RECURSOS: Array<RecursoType> = [
+  { nome: "Ação", quantidade: 1 },
+  { nome: "Madeira", quantidade: 0 },
+];
 
 export default function ListaDeRecursos() {
   return (
     <dl className="recursos">
-      <div>
-        <dt>Ação</dt>
-        <dd>1</dd>
-      </div>
-      <div>
-        <dt>Madeira</dt>
-        <dd>0</dd>
-      </div>
+      {RECURSOS.map((r) => (
+        <div>
+          <dt>{r.nome}</dt>
+          <dd>{r.quantidade}</dd>
+        </div>
+      ))}
     </dl>
   );
 }
