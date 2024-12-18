69dab71df1f141605a73d7e08a44485f4392bd33
Isabela Coelho <isabelacoelhoo@gmail.com>
2024-12-18 16:59:44 -0300

    adiciona key na lista de recursos

--- src/ListaDeRecursos.tsx
+++ src/ListaDeRecursos.tsx
@@ -7,7 +7,7 @@ export default function ListaDeRecursos({recursos}:RecursosProps) {
   return (
     <dl className="recursos">
       {recursos.map((r) => (
-        <div>
+        <div key= {r.nome}>
           <dt>{r.nome}</dt>
           <dd>{r.quantidade}</dd>
         </div>
