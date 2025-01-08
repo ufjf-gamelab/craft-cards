0b7091b95b3e25542b66ae2c9ec0c7896a379c70
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-01-08 17:00:32 -0300

    adiciona gestão de recursos

--- src/App.tsx
+++ src/App.tsx
@@ -18,14 +18,14 @@ export type RecursoType = {
 };
 
 const RECURSOS: Array<RecursoType> = [
-  { nome: "Ação", quantidade: 1 },
-  { nome: "Madeira", quantidade: 0 },
+  { nome: "ação", quantidade: 1 },
+  { nome: "madeira", quantidade: 0 },
 ];
 
 const MAO:Array<CartaType> = [
   {id:"m1", titulo: "Pegar madeira",texto:"", custo: 0, ganho: [{nome:"madeira", quantidade: 1}]},
   {id:"a1", titulo: "Pegar água",texto:"", custo: 0, ganho: [{nome:"água", quantidade: 1}]},
-  {id:"a2", titulo: "Pegar água",texto:"", custo: 0, ganho: [{nome:"água", quantidade: 1}]},
+  {id:"a2", titulo: "Beber água",texto:"", custo: 0, ganho: [{nome:"água", quantidade: -1}, {nome: "ação", quantidade: 1}]},
   {id:"p1", titulo: "Pegar pedra",texto:"", custo: 1, ganho: [{nome:"pedra", quantidade: 1}]},
   {id:"b1", titulo: "Pegar amora",texto:"", custo: 2, ganho: [{nome:"amora", quantidade: 1}]},
   {id:"m2", titulo: "Pegar madeira",texto:"", custo: 0, ganho: [{nome:"madeira", quantidade: 2}]},
@@ -48,7 +48,24 @@ function App() {
 
   function onCartaClick(carta:CartaType){
     console.log("carta clicada", carta);
-    setRecursos([...recursos, ...carta.ganho]);
+
+    const newRecursos = structuredClone(recursos);
+
+    carta.ganho.forEach((ganho)=>{
+      const recurso = newRecursos.find(r=>r.nome===ganho.nome);
+      if(recurso){
+        recurso.quantidade += ganho.quantidade;
+      }
+      else{
+        newRecursos.push(structuredClone(ganho));
+      }
+    });
+    if(newRecursos.some(r=>r.quantidade<0)){
+      console.log("Não pode jogar essa carta");
+      return;
+    }
+
+    setRecursos(newRecursos);
   }
 
   return (
