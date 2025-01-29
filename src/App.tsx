beadd12b030099976f1e0e250398ae9abbc9d6c9
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-01-29 16:22:06 -0300

    extrai dados iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -3,74 +3,9 @@ import "./App.css";
 import Carta from "./Carta";
 import ListaDeRecursos from "./ListaDeRecursos";
 import MaoDeCartas from "./MaoDeCartas";
+import {CartaType, DESCARTE, MAO, RECURSOS} from "./data/cartas.ts";
 
-export type CartaType = {
-  id: string;
-  titulo: string;
-  texto: string;
-  custo: number;
-  ganho: Array<RecursoType>;
-};
 
-export type RecursoType = {
-  nome: string;
-  quantidade: number;
-};
-
-const RECURSOS: Array<RecursoType> = [
-  { nome: "ação", quantidade: 1 },
-  { nome: "madeira", quantidade: 0 },
-];
-
-const DESCARTE: Array<CartaType> = [];
-
-const MAO: Array<CartaType> = [
-  {
-    id: "m1",
-    titulo: "Pegar madeira",
-    texto: "",
-    custo: 0,
-    ganho: [{ nome: "madeira", quantidade: 1 }],
-  },
-  {
-    id: "a1",
-    titulo: "Pegar água",
-    texto: "",
-    custo: 0,
-    ganho: [{ nome: "água", quantidade: 1 }],
-  },
-  {
-    id: "a2",
-    titulo: "Beber água",
-    texto: "",
-    custo: 0,
-    ganho: [
-      { nome: "água", quantidade: -1 },
-      { nome: "ação", quantidade: 1 },
-    ],
-  },
-  {
-    id: "p1",
-    titulo: "Pegar pedra",
-    texto: "",
-    custo: 1,
-    ganho: [{ nome: "pedra", quantidade: 1 }],
-  },
-  {
-    id: "b1",
-    titulo: "Pegar amora",
-    texto: "",
-    custo: 2,
-    ganho: [{ nome: "amora", quantidade: 1 }],
-  },
-  {
-    id: "m2",
-    titulo: "Pegar madeira",
-    texto: "",
-    custo: 0,
-    ganho: [{ nome: "madeira", quantidade: 2 }],
-  },
-];
 
 function App() {
   const [pontos, setPontos] = useState(0);
---
+++ src/data/cartas.ts
@@ -0,0 +1,69 @@
+export type CartaType = {
+  id: string;
+  titulo: string;
+  texto: string;
+  custo: number;
+  ganho: Array<RecursoType>;
+};
+
+export type RecursoType = {
+  nome: string;
+  quantidade: number;
+};
+
+
+export const RECURSOS: Array<RecursoType> = [
+  { nome: "ação", quantidade: 1 },
+  { nome: "madeira", quantidade: 0 },
+];
+
+export const DESCARTE: Array<CartaType> = [];
+
+
+export const MAO: Array<CartaType> = [
+  {
+    id: "m1",
+    titulo: "Pegar madeira",
+    texto: "",
+    custo: 0,
+    ganho: [{ nome: "madeira", quantidade: 1 }],
+  },
+  {
+    id: "a1",
+    titulo: "Pegar água",
+    texto: "",
+    custo: 0,
+    ganho: [{ nome: "água", quantidade: 1 }],
+  },
+  {
+    id: "a2",
+    titulo: "Beber água",
+    texto: "",
+    custo: 0,
+    ganho: [
+      { nome: "água", quantidade: -1 },
+      { nome: "ação", quantidade: 1 },
+    ],
+  },
+  {
+    id: "p1",
+    titulo: "Pegar pedra",
+    texto: "",
+    custo: 1,
+    ganho: [{ nome: "pedra", quantidade: 1 }],
+  },
+  {
+    id: "b1",
+    titulo: "Pegar amora",
+    texto: "",
+    custo: 2,
+    ganho: [{ nome: "amora", quantidade: 1 }],
+  },
+  {
+    id: "m2",
+    titulo: "Pegar madeira",
+    texto: "",
+    custo: 0,
+    ganho: [{ nome: "madeira", quantidade: 2 }],
+  },
+];
