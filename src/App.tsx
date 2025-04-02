379406655305246c78326b6dddef38c5dc63a506
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-04-02 11:18:58 -0300

    refatora Jogador e App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import "./App.css";
-import Carta from "./Carta";
 import ListaDeRecursos from "./ListaDeRecursos";
-import ListaDeCartas from "./ListaDeCartas.tsx";
-import { CartaType } from "./data/cartas.ts";
 import {
   GameActions,
   GameDispatchContext,
   GameReducerContext
 } from "./Game.ts";
-import Baralho from "./Baralho.tsx";
-import Descarte from "./Descarte.tsx";
 import { useContext } from "react";
 import Oferta from "./Oferta.tsx";
+import Jogador from "./Jogador.tsx";
 
 function App() {
   const game = useContext(GameReducerContext)!;
@@ -28,16 +24,8 @@ function App() {
     dispatch({ type: GameActions.DIMINUI_ACAO });
   }
 
-  function onCartaClick(carta: CartaType) {
-    dispatch({ type: GameActions.JOGAR_CARTA, carta });
-  }
-
   function passarTurno() {
     dispatch({ type: GameActions.PASSAR_TURNO });
-  }
-
-  function onDescarteClick() {
-    console.log("teste");
   }
 
   return (
@@ -48,29 +36,7 @@ function App() {
         <button onClick={passarTurno}>Passar Turno</button>
         <ListaDeRecursos recursos={game.recursos} />
         <Oferta/>
-        <h2>Mão</h2>
-        <ListaDeCartas>
-          {game.mao.map((item) => (
-            <Carta
-              key={item.id}
-              carta={item}
-              onCartaClick={() => {
-                onCartaClick(item);
-              }}
-            />
-          ))}
-        </ListaDeCartas>
-        <h2>Em Jogo</h2>
-        <ListaDeCartas>
-          {game.emJogo.map((item) => (
-            <Carta key={item.id} carta={item} onCartaClick={() => {}} />
-          ))}
-        </ListaDeCartas>
-        <h2>Baralho</h2>
-        <div className="baralho-container">
-          <Baralho cartas={game.baralho} />
-          <Descarte onDescarteClick={onDescarteClick} cartas={game.descarte} />
-        </div>
+        <Jogador/>
       </>
   );
 }
