8c509d3ec49237fbad490909d25d7dce254c1ca0
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-04-02 10:43:02 -0300

    adiciona contextos

--- src/App.tsx
+++ src/App.tsx
@@ -3,108 +3,105 @@ import "./App.css";
 import Carta from "./Carta";
 import ListaDeRecursos from "./ListaDeRecursos";
 import ListaDeCartas from "./ListaDeCartas.tsx";
-import {CartaType,GAME_INITIAL} from "./data/cartas.ts";
-import { GameActions, gameReducer, setupNewGame } from "./Game.ts";
+import { CartaType, GAME_INITIAL } from "./data/cartas.ts";
+import {
+  GameActions,
+  GameDispatchContext,
+  gameReducer,
+  GameReducerContext,
+  setupNewGame,
+} from "./Game.ts";
 import Baralho from "./Baralho.tsx";
 import Descarte from "./Descarte.tsx";
 
-
-
 function App() {
-
-  const [game,dispatch] = useReducer(gameReducer, GAME_INITIAL, setupNewGame);
+  const [game, dispatch] = useReducer(gameReducer, GAME_INITIAL, setupNewGame);
 
   function aumentaPonto() {
-    dispatch({type: GameActions.AUMENTA_PONTO});
+    dispatch({ type: GameActions.AUMENTA_PONTO });
   }
 
   function diminuiAcao() {
-    dispatch({type: GameActions.DIMINUI_ACAO});
+    dispatch({ type: GameActions.DIMINUI_ACAO });
   }
 
   function onCartaClick(carta: CartaType) {
-    dispatch({type: GameActions.JOGAR_CARTA, carta});
+    dispatch({ type: GameActions.JOGAR_CARTA, carta });
   }
 
-  function passarTurno(){
-    dispatch({type: GameActions.PASSAR_TURNO});
+  function passarTurno() {
+    dispatch({ type: GameActions.PASSAR_TURNO });
   }
 
   function onCompraCartaClick(carta: CartaType) {
-    dispatch({type: GameActions.COMPRAR_CARTA, carta});
+    dispatch({ type: GameActions.COMPRAR_CARTA, carta });
   }
 
-  function onDescarteClick(){
-    console.log('teste');
+  function onDescarteClick() {
+    console.log("teste");
   }
 
   return (
-    <>
-      <div>pontos: {game.pontos}</div>
-      <button onClick={aumentaPonto}>Aumenta Ponto</button>
-      <button onClick={diminuiAcao}>Diminui Ação</button>
-      <button onClick={passarTurno}>Passar Turno</button>
-      <ListaDeRecursos recursos={game.recursos} />
-      
-      <h2>Baralho da Oferta</h2>
-      <div className="baralho-container">
-        <Baralho cartas={game.baralhoDaOferta}/>
-        <Descarte onDescarteClick={onDescarteClick} cartas={game.descarteDaOferta}/>
-      </div>
+    <GameReducerContext.Provider value={game}>
+      <GameDispatchContext.Provider value={dispatch}>
+        <div>pontos: {game.pontos}</div>
+        <button onClick={aumentaPonto}>Aumenta Ponto</button>
+        <button onClick={diminuiAcao}>Diminui Ação</button>
+        <button onClick={passarTurno}>Passar Turno</button>
+        <ListaDeRecursos recursos={game.recursos} />
 
-      <h2>Oferta</h2>
-      <ListaDeCartas>
-        {game.oferta.map((item) => (
-          <Carta
-            key={item.id}
-            carta={item}
-            onCartaClick={() => {
-              onCompraCartaClick(item);
-            }}
+        <h2>Baralho da Oferta</h2>
+        <div className="baralho-container">
+          <Baralho cartas={game.baralhoDaOferta} />
+          <Descarte
+            onDescarteClick={onDescarteClick}
+            cartas={game.descarteDaOferta}
           />
-        ))}
-      </ListaDeCartas>
-      <h2>Descarte da Oferta</h2>
-      <ListaDeCartas>
-        {game.descarteDaOferta.map((item) => (
-          <Carta
-            key={item.id}
-            carta={item}
-            onCartaClick={() => {
-              
-            }}
-          />
-        ))}
-      </ListaDeCartas>
-      <h2>Mão</h2>
-      <ListaDeCartas>
-        {game.mao.map((item) => (
-          <Carta
-            key={item.id}
-            carta={item}
-            onCartaClick={() => {
-              onCartaClick(item);
-            }}
-          />
-        ))}
-      </ListaDeCartas>
-      <h2>Em Jogo</h2>
-      <ListaDeCartas>
-        {game.emJogo.map((item) => (
-          <Carta
-            key={item.id}
-            carta={item}
-            onCartaClick={() => {
-            }}
-          />
-        ))}
-      </ListaDeCartas>
-      <h2>Baralho</h2>
-      <div className="baralho-container">
-        <Baralho cartas={game.baralho}/>
-        <Descarte onDescarteClick={onDescarteClick} cartas={game.descarte}/>
-      </div>
-    </>
+        </div>
+
+        <h2>Oferta</h2>
+        <ListaDeCartas>
+          {game.oferta.map((item) => (
+            <Carta
+              key={item.id}
+              carta={item}
+              onCartaClick={() => {
+                onCompraCartaClick(item);
+              }}
+            />
+          ))}
+        </ListaDeCartas>
+        <h2>Descarte da Oferta</h2>
+        <ListaDeCartas>
+          {game.descarteDaOferta.map((item) => (
+            <Carta key={item.id} carta={item} onCartaClick={() => {}} />
+          ))}
+        </ListaDeCartas>
+        <h2>Mão</h2>
+        <ListaDeCartas>
+          {game.mao.map((item) => (
+            <Carta
+              key={item.id}
+              carta={item}
+              onCartaClick={() => {
+                onCartaClick(item);
+              }}
+            />
+          ))}
+        </ListaDeCartas>
+        <h2>Em Jogo</h2>
+        <ListaDeCartas>
+          {game.emJogo.map((item) => (
+            <Carta key={item.id} carta={item} onCartaClick={() => {}} />
+          ))}
+        </ListaDeCartas>
+        <h2>Baralho</h2>
+        <div className="baralho-container">
+          <Baralho cartas={game.baralho} />
+          <Descarte onDescarteClick={onDescarteClick} cartas={game.descarte} />
+        </div>
+      </GameDispatchContext.Provider>
+    </GameReducerContext.Provider>
   );
 }
 
