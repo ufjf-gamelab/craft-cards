4f090a5a065bfd65da46320f08a03d0975b8ed02
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-05-12 14:20:29 -0300

    adiciona animações basicas em cartas

--- src/Carta.tsx
+++ src/Carta.tsx
@@ -1,9 +1,16 @@
 import { CartaType } from "./data/cartas.ts";
 import "./Carta.css";
+import * as motion from "motion/react-client"
 type CartaProps = { carta: CartaType; onCartaClick: () => void };
 export default function Carta({ carta, onCartaClick }: CartaProps) {
   return (
-    <div
+    <motion.div
+    initial={{ opacity: 0, scale: 0 }}
+    animate={{ opacity: 1, scale: 1 }}
+    transition={{
+        duration: 0.4,
+        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+    }}
       className="carta"
       onClick={() => {
         onCartaClick();
@@ -25,6 +32,6 @@ export default function Carta({ carta, onCartaClick }: CartaProps) {
             {ganho.nome}
           </p>
         ))}</div>
-    </div>
+    </motion.div>
   );
 }
