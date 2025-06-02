4f2a320836ec3a99e781bc20066fba4af90d2504
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-06-02 14:29:15 -0300

    Revert "adiciona passarRef para pegar a posição do elemento"
    This reverts commit 9cfc00dffe66a87c165625eb329e60c68e023897.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {
   GameDispatchContext,
   GameReducerContext
 } from "./Game.ts";
-import { useContext, useRef } from "react";
+import { useContext } from "react";
 import Oferta from "./Oferta.tsx";
 import Jogador from "./Jogador.tsx";
 
@@ -24,18 +24,16 @@ function App() {
     dispatch({ type: GameActions.DIMINUI_ACAO });
   }
 
-  const passarRef = useRef<HTMLButtonElement>(null);
   function passarTurno() {
     dispatch({ type: GameActions.PASSAR_TURNO });
-    console.log(passarRef.current?.getBoundingClientRect());
   }
-  
+
   return (
     <>
         <div>pontos: {game.pontos}</div>
         <button onClick={aumentaPonto}>Aumenta Ponto</button>
         <button onClick={diminuiAcao}>Diminui Ação</button>
-        <button onClick={passarTurno} ref = {passarRef}>Passar Turno</button>
+        <button onClick={passarTurno}>Passar Turno</button>
         <ListaDeRecursos recursos={game.recursos} />
         <Oferta/>
         <Jogador/>
